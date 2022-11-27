ef4da52ae5463e0ba02da4e3e46d9f096a34fe52
cha <chyb627@naver.com>
2022-11-27 19:02:29 +0900

    변수, 함수, 객체, 배열 타입지정

--- first.ts
+++ first.ts
@@ -1,2 +1,32 @@
-let a: string = 'hello ts';
-a = 1234;
+// 변수
+const a: number = 5;
+const b: string = 'hi';
+const c: boolean = true;
+const d: undefined = undefined;
+const e: null = null;
+const f: any = 'kkkk'; // 모든게 다됨. 의미가 없음. 자바스크립트임.
+const g: true = true;
+const h: 5 = 5;
+
+// 함수
+function add(x: number, y: number): number {return x + y};
+
+const arrowAdd: (x: number, y: number) => number = (x, y) => x + y;
+
+type ArrowFunc = (x: number, y: number) => number;
+const arrowFunc: ArrowFunc = (x, y) => x + y;
+
+interface MakeFunc {
+    (x: number, y: number): number;
+}
+const makeFunc: MakeFunc = (x, y) => x + y;
+
+// 객체
+const obj: {lat: number, lon: number} = {lat: 37.5, lon: 127.5};
+
+// 배열
+const arr: string[] = ['123', '456'];
+const arr2: Array<number> = [123, 456];
+
+// 튜플 (길이가 고정된 배열)
+const arr3: [number, number, string] = [123, 456, 'hello'];
