d3d4c6bf378338c82f2fb01d6109af3888adce1d
cha <chyb627@naver.com>
2022-12-07 16:06:30 +0900

    타입 애일리어스와 인터페이스의 상속(extends)

--- first.ts
+++ first.ts
@@ -9,7 +9,9 @@ const g: true = true;
 const h: 5 = 5;
 
 // 함수
-function add(x: number, y: number): number {return x + y};
+function add(x: number, y: number): number {
+  return x + y;
+}
 
 const arrowAdd: (x: number, y: number) => number = (x, y) => x + y;
 
@@ -17,19 +19,19 @@ type ArrowFunc = (x: number, y: number) => number;
 const arrowFunc: ArrowFunc = (x, y) => x + y;
 
 interface MakeFunc {
-    (x: number, y: number): number;
+  (x: number, y: number): number;
 }
 const makeFunc: MakeFunc = (x, y) => x + y;
 
 function add2(x: number, y: number): number;
 function add2(x, y) {
-    return x + y;
+  return x + y;
 }
 
 // 객체
-const obje: {lat: number, lon: number} = {lat: 37.5, lon: 127.5};
+const obje: { lat: number; lon: number } = { lat: 37.5, lon: 127.5 };
 
-const obj = {lat: 37.5, lon: 127.5} as const;
+const obj = { lat: 37.5, lon: 127.5 } as const;
 type Key = keyof typeof obj;
 type Value = typeof obj[keyof typeof obj];
 
@@ -41,7 +43,7 @@ const arr2: Array<number> = [123, 456];
 const arr3: [number, number, string] = [123, 456, 'hello'];
 
 // type Greeting = "hello world"
-type World = "world";
+type World = 'world';
 const aa: World = 'world';
 
 const bb = `hello ${aa}`;
@@ -49,27 +51,27 @@ type Greeting = `hello ${World}`;
 
 // rest parameter
 function rest(a, ...args: string[]) {
-    console.log(a, args);   // 1, [2,3]
+  console.log(a, args); // 1, [2,3]
 }
-rest('1','2','3');
+rest('1', '2', '3');
 
 // enum
 const enum EDirection {
-    Up = 3,
-    Down = 5,
-    Left = 4,
-    Right = 6,
+  Up = 3,
+  Down = 5,
+  Left = 4,
+  Right = 6,
 }
 
 const ODirection = {
-    Up: 0,
-    Down: 1,
-    Left: 2,
-    Right: 3,
+  Up: 0,
+  Down: 1,
+  Left: 2,
+  Right: 3,
 } as const;
 
-const aaa = EDirection.Up;   // 3
-const ccc = EDirection.Left;  // 4
+const aaa = EDirection.Up; // 3
+const ccc = EDirection.Left; // 4
 
 function walk(dir: EDirection) {}
 
@@ -78,3 +80,35 @@ function run(dir: Direction) {}
 
 walk(EDirection.Left);
 run(ODirection.Right);
+
+// &는 둘다 만족시켜야함. 모든 속성이 다 있어함. intersection
+type A2 = { hello: 'world' } & { younbin: 'cha' };
+const a2: A2 = { hello: 'world', younbin: 'cha' };
+
+// 타입 애일리어스와 인터페이스의 상속(extends)
+type Animal = { breath: true };
+type Mammalia = Animal & { bread: true };
+type Human = Mammalia & { think: true };
+
+interface Animall {
+  breath: true;
+}
+interface Mammaliaa extends Animall {
+  bread: true;
+}
+
+const youngbin: Human = { breath: true, bread: true, think: true };
+
+interface O {
+  talk: () => void;
+}
+interface O {
+  eat: () => void;
+}
+interface O {
+  shit: () => void;
+}
+
+const o: O = { talk() {}, eat() {}, shit() {}, sleep() {} };
+// interface는 합쳐지는 특성이 있다.
+// 이 특성을 기반으로 남의 라이브러리 코드를 수정한다.
